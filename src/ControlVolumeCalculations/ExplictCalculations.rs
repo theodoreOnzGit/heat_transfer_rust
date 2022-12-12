bc8d78b77c4fe81b146edda5ca9ee9ed3adcf913
tedGuest@ideapadRyzenLaptop <theodore_ong@berkeley.edu>
2022-12-12 10:03:05 -0800

    cleaned up explicit calculations

--- src/ControlVolumeCalculations/ExplictCalculations.rs
+++ src/ControlVolumeCalculations/ExplictCalculations.rs
@@ -2,22 +2,3 @@ extern crate uom;
 use uom::si::f64::*;
 
 
-pub fn get_control_volume_enthalpy_next_timestep(
-    timestep: Time,
-    enthalpy_out: Power,
-    enthalpy_in: Power,
-    heat_supplied_to_system: Power,
-    work_done_on_system: Power,
-    control_volume_enthalpy_current_timestep: Energy,
-    ) -> Energy {
-
-    let control_volume_enthalpy_next_timestep: Energy 
-        = timestep * (enthalpy_in - 
-                      enthalpy_out +
-                      heat_supplied_to_system +
-                      work_done_on_system) 
-        + control_volume_enthalpy_current_timestep;
-
-    return control_volume_enthalpy_next_timestep;
-
-}
