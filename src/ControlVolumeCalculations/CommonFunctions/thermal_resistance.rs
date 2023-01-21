c3a770b2b3afe369c110ba32e18a3a2fb7069689
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2023-01-21 02:38:24 -0800

    added one power through convection thermal resistance

--- src/ControlVolumeCalculations/CommonFunctions/thermal_resistance.rs
+++ src/ControlVolumeCalculations/CommonFunctions/thermal_resistance.rs
@@ -21,7 +21,7 @@
 //! 
 //! Q/A = - (Delta T) h
 //!
-//! (Delta T)/Q = h/A
+//! (Delta T)/Q = 1/(hA)
 //!
 //! The unit for thermal resistance here is kelvin per watt
 //! 
@@ -34,6 +34,44 @@
 //!
 //!
 use uom::si::f64::*;
+use uom::si::power::watt;
+
+/// calcualtes heat flow using a thermal resistance model,
+/// Q/A = - (Delta T) h
+///
+/// thermal resistance is:
+/// (Delta T)/Q = 1/(hA)
+pub fn obtain_power_through_single_convection_thermal_resistance(
+    temperature_of_heat_recipient: ThermodynamicTemperature,
+    temperature_of_heat_source: ThermodynamicTemperature,
+    average_surface_area: Area,
+    heat_transfer_coefficient: HeatTransfer,
+    ) -> Power {
+
+
+    // thermal resistance
+    //
+    
+
+    let thermal_resistance = 
+        1.0_f64
+        /average_surface_area
+        /heat_transfer_coefficient;
+
+    use super::*;
+    // -Delta T
+    let temperature_interval = 
+        -subtract_two_thermodynamic_temperatures(
+            temperature_of_heat_recipient,
+            temperature_of_heat_source);
+
+
+    let heat_flow: Power = 
+        temperature_interval
+        /thermal_resistance;
+
+    return heat_flow;
+}
 
 /// calcualtes heat flow using a thermal resistance model,
 /// -(Delta T)/Q = (Delta x)/(kA)
