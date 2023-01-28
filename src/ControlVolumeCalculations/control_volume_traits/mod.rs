7728376e15b5ae413a1a2feb589d692761678fa4
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2023-01-28 15:06:02 -0800

    added basic methods for control volume trait

--- src/ControlVolumeCalculations/control_volume_traits/mod.rs
+++ src/ControlVolumeCalculations/control_volume_traits/mod.rs
@@ -19,7 +19,91 @@
 
 use uom::si::f64::*;
 use super::CommonFunctions::*;
-pub trait ControlVolume {}
+
+/// this is the primary trait for control volume,
+/// meant for making trait objects
+///
+/// it is tightly coupled to the fluid_mechanics_rust package
+/// because it is meant to return
+/// the thermophysical properties struct
+pub trait ControlVolume: ControlVolumeAssociatedFunctions {
+
+    // for each calculation, we get our control volume enthalpy
+
+    /// gets the control volume specific enthalpy
+    fn get_control_volume_specific_enthalpy(&self) -> AvailableEnergy;
+
+    /// gets the control volume mass
+    fn get_control_volume_mass(&self) -> Mass;
+
+    /// get sum of enthalpy flows in
+    fn get_sum_of_enthalpy_flows_in(&self) -> Power;
+
+    /// get sum of enthalpy flows out
+    fn get_sum_of_enthalpy_flows_out(&self) -> Power;
+
+    /// get_heat_supplied_to_system
+    fn get_heat_supplied_to_system(&self) -> Power;
+
+    /// get work done on system
+    fn get_work_done_on_system(&self) -> Power;
+
+    /// convert enthalpy to temperature
+    fn get_fluid_temperature_from_specific_enthalpy(
+        &self,
+        fluid_enthalpy: AvailableEnergy) -> ThermodynamicTemperature;
+
+
+    /// calculate next timestep control volume temperature
+    ///
+    /// I leave timestep here as an input because
+    /// we may want to have adjustable timestep
+    fn calculate_control_vol_temp_next_timestep(
+        &self,
+        timestep: Time) -> ThermodynamicTemperature{
+
+        let current_timestep_control_volume_specific_enthalpy 
+            = self.get_control_volume_specific_enthalpy();
+
+        let current_timestep_control_volume_mass  
+            = self.get_control_volume_mass();
+
+        let enthalpy_in 
+            = self.get_sum_of_enthalpy_flows_in();
+
+        let enthalpy_out 
+            = self.get_sum_of_enthalpy_flows_out();
+
+        let heat_supplied_to_system 
+            = self.get_heat_supplied_to_system();
+
+        let work_done_on_system
+            = self.get_work_done_on_system();
+
+        // we may want to change this in future to involve Courant
+        // number calcs
+        let next_timestep_specific_enthalpy
+            = Self::calculate_specific_enthalpy_at_next_timestep(
+                current_timestep_control_volume_specific_enthalpy, 
+                current_timestep_control_volume_mass, 
+                timestep, 
+                enthalpy_out, 
+                enthalpy_in, 
+                heat_supplied_to_system, 
+                work_done_on_system);
+
+
+
+        let next_timestep_control_vol_temperature
+            = self.get_fluid_temperature_from_specific_enthalpy(
+                next_timestep_specific_enthalpy.unwrap());
+
+        return next_timestep_control_vol_temperature;
+
+    }
+
+    
+}
 
 /// contains associated functions for the control
 /// volume,
@@ -31,6 +115,8 @@ pub trait ControlVolumeAssociatedFunctions {
     ///
     /// this assumes that the mass of the control volume does not change
     /// in any significant way
+    ///
+    /// this function does not check for Courant number
     fn calculate_specific_enthalpy_at_next_timestep(
         current_timestep_control_volume_specific_enthalpy: AvailableEnergy,
         current_timestep_control_volume_mass: Mass,
@@ -61,7 +147,6 @@ pub trait ControlVolumeAssociatedFunctions {
 
         return Ok(specific_enthalpy_next_timestep);
     }
-
     
 
 }
